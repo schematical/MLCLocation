30ef35735a6f20240c5f3929b985549ee27e109c
Matt Lea <github@mattleaconsulting.com>
2013-04-25 09:09:07 -0500

    fixing save controls

--- _core/ctl/MLCLocationEditPanel.class.php
+++ _core/ctl/MLCLocationEditPanel.class.php
@@ -12,7 +12,19 @@ class MLCLocationEditPanel extends MLCLocationEditPanelBase {
         $this->strTemplate = __MLC_LOCATION_CORE_VIEW__ . '/MLCLocationEditPanel.tpl.php';
     }
     public function btnSave_click(){
-        parent::btnSave_click();
+        if (is_null($this->objMLCLocation)) {
+            //Create a new one
+            $this->objMLCLocation = new MLCLocation();
+        }
+        $this->objMLCLocation->shortDesc = $this->txtShortDesc->Text;
+        $this->objMLCLocation->address1 = $this->txtAddress1->Text;
+        $this->objMLCLocation->address2 = $this->txtAddress2->Text;
+        $this->objMLCLocation->city = $this->txtCity->Text;
+        $this->objMLCLocation->state = $this->txtState->Text;
+        $this->objMLCLocation->zip = $this->txtZip->Text;
+        $this->objMLCLocation->country = $this->txtCountry->Text;
+        $this->objMLCLocation->idAccount = MLCAuthDriver::IdAccount();
+        $this->objMLCLocation->Save();
         $this->objLocation->GetLatLng();
     }
 
